f072d03b0197e649881bee4aecda6aa23ce92b91
Daniel Escobar <daanielescobar@gmail.com>
2022-09-08 01:30:15 -0500

    dont actually know what changed here

--- transaction.py
+++ transaction.py
@@ -7,3 +7,17 @@ url = os.getenv("url")
 key = os.getenv("public")
 
 supabase: Client = create_client(url, key)
+
+def new_transaction(sent_by, received_by, amount, transaction_type):
+    supabase.table("transaction").insert({
+            "transaction_type": transaction_type, 
+            "amount" : amount, 
+            "sent_by": sent_by, 
+            "received_by": received_by
+    }).execute()
+
+def insert_welcome_gift_transaction(sent_by, received_by, amount):
+    new_transaction(sent_by=sent_by, received_by=received_by, amount=amount, transaction_type="welcome_gift")
+
+def insert_promo_reward_transaction(sent_by, received_by, amount, transaction_type):
+    new_transaction(sent_by=sent_by, received_by=received_by, amount=amount, transaction_type=transaction_type)
