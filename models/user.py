6dbeb0cca96aa968393bced3d8b0ae9e99b2a5ab
Daniel Escobar <daanielescobar@gmail.com>
2022-12-17 01:25:52 -0600

    postgres py has made unusable the .eq() function, replace them with .filter()

--- models/user.py
+++ models/user.py
@@ -9,11 +9,10 @@ class User():
         data = self.get_user_coins(discord_name=discord_name, discord_id=discord_id)
         balance = data.data[0]["coins"]        
         new_balance = operation(balance, price)
-        self.users.update({"coins": int(new_balance)}).eq(column="discordUser", value=discord_name).execute()
+        self.users.update({"coins": int(new_balance)}).filter("discordUser", "eq", discord_name).execute()
     
     def find_or_create_user(self, discord_id, discord_name):
-
-        data = self.users.select("*").eq("discord_id", discord_id).execute()
+        data = self.users.select("*").filter("discord_id", "eq", discord_id).execute()
 
         user_exists_and_has_id = len(data.data) > 0
         
@@ -21,19 +20,19 @@ class User():
             print("User has a discord_user and discord_Id")
             return
         else:
-            data = self.users.select("*").eq("discordUser", discord_name).execute()
+            data = self.users.select("*").filter("discordUser", "eq", discord_name).execute()
             user_exists_but_has_no_id = len(data.data) > 0
             
             if user_exists_but_has_no_id:
                 print("Inserting dicord id to existing user.")
-                self.users.update({"discord_id": discord_id}).eq("discordUser", discord_name).execute()
+                self.users.update({"discord_id": discord_id}).filter("discordUser", "eq", discord_name).execute()
                 return
             else:
                 self.users.insert({"discord_id": discord_id, "discordUser": discord_name}).execute()
     
     def get_user_coins(self, discord_name, discord_id):
         self.find_or_create_user(discord_name=discord_name, discord_id=discord_id)
-        coins = self.users.select("coins").eq("discordUser", discord_name).execute()
+        coins = self.users.select("coins").filter("discordUser", "eq", discord_name).execute()
         return coins
 
     def get_user_promos(self, discord_id, discord_name):
